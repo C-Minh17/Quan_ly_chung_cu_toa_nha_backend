c255ffd2ace77442f45cb3577358ed69a2967935
congMinh <minh.ptit.k6@gmail.com>
2026-03-28 01:33:06 +0700

    lỗi văng web

--- src/routes/user/profile.route.js
+++ src/routes/user/profile.route.js
@@ -3,13 +3,14 @@ import * as profileController from '@/app/controllers/user/profile.controller'
 import validate from '@/app/middleware/user/validate'
 import * as profileRequest from '@/app/requests/user/profile.request'
 import { checkValidToken } from '@/app/middleware/user/auth.middleware'
+import { asyncHandler } from '@/utils/helpers'
 
 const router = express.Router()
 
-router.get('/me', checkValidToken, profileController.getProfile)
+router.get('/me', asyncHandler(checkValidToken), asyncHandler(profileController.getProfile))
 router.put('/me', [
-    checkValidToken,
-    validate(profileRequest.updateProfile)
-], profileController.updateProfile)
+    asyncHandler(checkValidToken),
+    asyncHandler(validate(profileRequest.updateProfile))
+], asyncHandler(profileController.updateProfile))
 
-export default router 
+export default router
